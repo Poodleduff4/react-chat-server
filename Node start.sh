b94ad165f18e32c7d1ca08e5328444fc6e6ff5a2
Poodleduff4 <lukeguardino@yahoo.ca>
2022-06-10 15:22:13 -0400

    m

---
+++ Node start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd ~/Documents/JS\ crap/react-chat-server/
+node "Back End/server.js"
+# osascript -e 'tell app "Terminal" to do script "cd Front\ End && npm start"'
---
+++ React start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd ~/Documents/JS\ crap/react-chat-server/Front\ End/
+npm start
